2a1bdc8a7a8ed492758b96dfdc88d7219d51e238
Chandler-Davidson <cdavidson@3-gis.com>
2020-07-31 12:08:19 -0500

    Add volume for shared static site build

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { RecordingsModule } from './recordings/recordings.module';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     TypeOrmModule.forRoot(),
     RecordingsModule,
   ],
